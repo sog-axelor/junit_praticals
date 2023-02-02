303c89be0f564870a63ffafd3d00a3a9848a7938
Sonu <s.gupta@axelor.com>
2023-02-02 16:36:38 +0530

    ignoreCAse

---
+++ src/main/java/com/unique/main/TestJunit.java
@@ -0,0 +1,27 @@
+package com.unique.main;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit {
+
+   String message = "Robert";	
+   MessageUtil messageUtil = new MessageUtil(message);
+   
+   @Ignore
+   @Test
+   public void testPrintMessage() {
+      System.out.println("Inside testPrintMessage()");
+      message = "Robert";
+      assertEquals(message,messageUtil.printMessage());
+   }
+
+   @Test
+   public void testSalutationMessage() {
+      System.out.println("Inside testSalutationMessage()");
+      message = "Hi!" + "Robert";
+      assertEquals(message,messageUtil.salutationMessage());
+   }
+	
+}
